6d3afaecc39db26be62c8c14a146518cbe2898cb
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-08-22 11:42:59 +0200

    feat: page title

--- src/app/pages/admin-page/schedule-page/schedule-page.component.ts
+++ src/app/pages/admin-page/schedule-page/schedule-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-schedule-page',
@@ -8,4 +9,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     styleUrl: './schedule-page.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SchedulePageComponent {}
+export class SchedulePageComponent {
+    @Input() set id(id: string) {
+        this.setTitle(id);
+    }
+
+    constructor(private readonly title: Title) {}
+
+    setTitle(id: string): void {
+        this.title.setTitle(`Route №${id}`);
+    }
+}
