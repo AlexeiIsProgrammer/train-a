e9d35eeae1b031c7c9d077c10efd7462d6c9ab87
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-08-28 21:28:01 +0200

    fix: .evn

--- src/app/shared/service/auth/token-names.const.ts
+++ src/app/shared/service/auth/token-names.const.ts
@@ -1,2 +1,2 @@
-export const ADMIN_TOKEN_SHORT_NAME = btoa('@admin').toLowerCase();
+export const ADMIN_TOKEN_SHORT_NAME = btoa(import.meta.env.NG_APP_ADMIN_SHORT_NAME).toLowerCase();
 export const accessTokenName = 'accessToken';
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,6 @@
 declare interface Env {
     readonly NG_APP_ADMINS: string;
+    readonly NG_APP_ADMIN_SHORT_NAME: string;
 }
 
 declare interface ImportMeta {
