a3d24975f81f1d2bb761a77438e4b7f4ce379f23
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-08-28 21:03:15 +0200

    feat: admin links

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,14 +2,14 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AuthService } from '@shared/service/auth/auth.service';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, AsyncPipe],
+    imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule, AsyncPipe],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
