d9930ed7bfaed4f8469205985bcefb0a13023048
Alexei <avenge.now.r@mail.ru>
2024-09-01 17:36:23 +0300

    feat: add profile-page route

--- src/app/router/app.routes.ts
+++ src/app/router/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from '@pages/page-not-found/page-not-found.component';
 import { isAdminGuard } from './guards/isAdmin/is-admin.guard';
 import { isGuestGuard } from './guards/isGuest/is-guest.guard';
+import { isUserGuard } from './guards/isUser/is-user.guard';
 
 export const routes: Routes = [
     {
@@ -9,6 +10,14 @@ export const routes: Routes = [
         loadChildren: () =>
             import('@pages/admin-page/admin.routes').then(({ AdminRoutes }) => AdminRoutes),
         canMatch: [isAdminGuard],
+    },
+    {
+        path: 'profile',
+        loadChildren: () =>
+            import('@pages/profile-page/profile-page.component').then(
+                ({ ProfilePageComponent }) => ProfilePageComponent,
+            ),
+        canMatch: [isUserGuard],
     },
     {
         path: 'signup',
