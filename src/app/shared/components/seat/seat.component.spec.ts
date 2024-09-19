e72314183d72e931dcb1a71d0ec59d4f4a0886e0
egorerkovich <erkovich.egor@yandex.by>
2024-09-19 12:55:43 +0300

    feat: add tests for carriages and orders

--- src/app/shared/components/seat/seat.component.spec.ts
+++ src/app/shared/components/seat/seat.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { SeatComponent } from './seat.component';
 
 describe('SeatComponent', () => {
@@ -18,5 +18,13 @@ describe('SeatComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should use large seat class when isSmallModel is false', () => {
+        component.isSmallModel = false;
+        fixture.detectChanges();
+
+        const seatButton = fixture.debugElement.query(By.css('button'));
+        expect(seatButton.nativeElement.classList).toContain('large-seat');
     });
 });
